e0c73258418647aef650e9cb9246cafb5c944e75
Bhumi Jain <bhumiijainn@gmail.com>
2026-06-03 16:18:00 +0530

    Update Progress.jsx

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,3 +1,6 @@
+import Image7 from '../Assets/Image7.png'
+import Image8 from '../Assets/Image8.png'
+
 function Arrow() {
   return (
     <a href="#" className="read-more">
@@ -9,7 +12,7 @@ function Arrow() {
 export default function Progress() {
   return (
     <section className="progress">
-      {/* Row 1: text left, image right */}
+
       <div className="split reveal">
         <div className="text">
           <h2>
@@ -17,45 +20,51 @@ export default function Progress() {
             <br />
             be better than today
           </h2>
+
           <p>
             We are a team of strategists, designers communicators, researchers.
             Together, we belive that progress only happpens when you allow to
             play things safe
           </p>
+
           <Arrow />
         </div>
+
         <div className="visual">
           <img
             className="circle-img"
-            src="src/Assets/Image7.png"
-            
+            src={Image7}
             alt="team collaborating"
           />
+
           <span className="tri tri-right" />
         </div>
       </div>
 
-      {/* Row 2: image left, text right */}
       <div className="split reverse reveal">
         <div className="visual">
           <img
             className="circle-img"
-            src="src/Assets/Image8.png"
+            src={Image8}
             alt="team working"
           />
+
           <span className="tri tri-bottom" />
         </div>
+
         <div className="text">
           <h2>
             <span className="hl-underline">See</span> how we can
             <br />
             help you progress
           </h2>
+
           <p>
             We add a layer of fearless insights and action that drives change
             makers to accelerate their progress in areas such as brand, design,
             digital, comms and social aspects.
           </p>
+
           <Arrow />
         </div>
       </div>
